a2ddfc6c8bbe017fac435902d77857f0d97729ab
Andrey-Vasilenko <avas@softserveinc.com>
2016-06-09 18:59:05 +0300

    SNAP-417 Update frontend

--- WebContent/js/controllers/modalSystemUninstallController.js
+++ WebContent/js/controllers/modalSystemUninstallController.js
@@ -4,10 +4,18 @@ angular.module('web')
     .controller('modalSystemUninstallCtrl', function ($scope, $modalInstance, System) {
         $scope.state = 'ask';
 
+        $scope.deletionOptions = [{
+            name: "Delete S3 bucket",
+            value: true
+        }, {
+            name: "Keep S3 bucket",
+            value: false
+        }];
+
         $scope.delete = function () {
             var deletionData = {
                 instanceId: $scope.instanceId,
-                removeS3Bucket: $scope.removeS3Bucket
+                removeS3Bucket: $scope.removeS3Bucket.value
             };
 
             System.delete(deletionData).then(function () {
