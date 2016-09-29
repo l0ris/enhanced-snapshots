76d0dc6a66d1782ae24a8c0c858d613e0b86f102
Irina Radaikina <iradaik@softserveinc.com>
2016-09-29 13:46:29 +0300

    SNAP-442: removed redundant comments, changed mode to prod

--- src/com/sungardas/enhancedsnapshots/ws/WebSocketConfig.java
+++ src/com/sungardas/enhancedsnapshots/ws/WebSocketConfig.java
@@ -13,12 +13,10 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     public static final String TASK_PROGRESS_DESTINATION = "/task";
     public static final String ERROR_DESTINATION = "/error";
     public static final String LOGS_DESTINATION = "/logs";
-    public static final String DESTINATION_PREFIX = "/app";
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker(ERROR_DESTINATION, TASK_PROGRESS_DESTINATION, LOGS_DESTINATION);
-//        config.setApplicationDestinationPrefixes(DESTINATION_PREFIX);
     }
 
     @Override
