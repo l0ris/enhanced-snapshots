6fcfb19466653242c93275d973e46348b52a9fa0
Irina Radaikina <iradaik@softserveinc.com>
2016-10-18 13:23:07 +0300

    SNAP-516: Non admin users should not have access to ESS logs endpoint
    Change-Id: I836083ff11be5ff3862575b0f8aa1eefa971383d

--- src/com/sungardas/enhancedsnapshots/rest/LogsController.java
+++ src/com/sungardas/enhancedsnapshots/rest/LogsController.java
@@ -3,20 +3,18 @@ package com.sungardas.enhancedsnapshots.rest;
 
 import com.sungardas.enhancedsnapshots.components.logwatcher.LogsWatcherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
-import java.util.Collection;
+import javax.annotation.security.RolesAllowed;
 
 @Controller
 public class LogsController {
 
-    private final static String SUBSCRIBTIONS_MSG = "subscribed";
-
     @Autowired
     private LogsWatcherService logsWatcherService;
 
+    @RolesAllowed("ROLE_ADMIN")
     @SubscribeMapping("/logs")
     public void subscriptionHandler() {
         logsWatcherService.start();
